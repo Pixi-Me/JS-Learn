5f79ec8c06c4b1eb234143a80bc808a16160a7d2
Pixi-Me <ezioboii@gmail.com>
2026-03-14 12:17:51 +0530

    Lunch Break

--- 5 Events/script.js
+++ 5 Events/script.js
@@ -1,8 +1,39 @@
-let inp = document.querySelector("#inp");
+// let inp = document.querySelector("#inp");
 
-inp.addEventListener('input',(i)=>{
-    if (i.data !== null) {
-        console.log(i.data)
-    }
+// let sel = document.querySelector("#sel");
+
+// inp.addEventListener('input',(i)=>{
+//     if (i.data !== null) {
+//         console.log(i.data);
+//     }
     
+// });
+
+// sel.addEventListener('change',(dets)=>{
+//     console.log(dets.target.value);
+// });
+
+
+//2
+// Displaying what u typed on keyboard
+
+// document.addEventListener('keydown',(dets)=>{
+//     console.log(dets);
+//     dets.key ===' ' ? document.querySelector('#txt').textContent= 'Space' : document.querySelector('#txt').textContent=dets.key;
+    
+
+// })
+
+
+//3 File Upload
+
+let upload = document.querySelector("#file");
+let btn = document.querySelector('#fileBtn');
+
+btn.addEventListener('click',()=>{
+    upload.click()
+})
+upload.addEventListener('change',(i)=>{
+    console.log()
+    btn.textContent=i.target.files[0].name
 })
